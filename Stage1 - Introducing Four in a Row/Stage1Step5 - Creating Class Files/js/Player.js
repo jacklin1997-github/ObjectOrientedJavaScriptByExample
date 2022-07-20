bacdb8f350f18b968e02fa93b30893c21c00d7f0
jacklin1997-github <jacklin1997@gmail.com>
2022-07-20 09:17:25 +0800

    create Player properties

--- Stage1 - Introducing Four in a Row/Stage1Step5 - Creating Class Files/js/Player.js
+++ Stage1 - Introducing Four in a Row/Stage1Step5 - Creating Class Files/js/Player.js
@@ -1,7 +1,11 @@
 class Player {
-  constructor(name, color, ID) {
+  constructor(name, color, ID, active=false) {
     this.name = name;
     this.color = color;
     this.ID = ID;
+    this.active = active;
+    this.token=[]
+//constructor args can put initial value
+//we can put properties that isn't exist in args
   }
 }
